f4e2e429e2e6570f29516e901f305e17cf7992c3
wuhibe <wuhibetamire710@gmail.com>
2021-06-17 12:51:25 -0700

    45

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /*
-*print a string
 *main - Start here
+*Return(0) - well done
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle""\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
