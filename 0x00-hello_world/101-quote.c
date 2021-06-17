cf7d54a4020dd583195b8335e26d2da9c2839548
wuhibe <wuhibetamire710@gmail.com>
2021-06-17 13:40:53 -0700

    final push? Hopefully

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * *main - Start here
+ *  *Return: Success prints 1
+ */
+int main(void)
+{
+write("and that piece of art is useful,\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
