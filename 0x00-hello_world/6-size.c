b86a414c6100785a92d91cbfe664795057f6789c
wuhibe <wuhibetamire710@gmail.com>
2021-06-17 13:30:21 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float e;
-puts("Size of a char: %d byte(s)", sizeof(a));
-puts("Size of an int: %d byte(s)", sizeof(b));
-puts("Size of a long int: %d byte(s)", sizeof(c));
-puts("Size of a long long int: %d byte(s)", sizeof(d));
-puts("Size of a float: %d byte(s)", sizeof(e));
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
 return (0);
 }
