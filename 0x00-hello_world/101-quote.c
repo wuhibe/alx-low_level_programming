9e2e6946dc0516bcfa5ed03d90f06894a14b643a
wuhibe <wuhibetamire710@gmail.com>
2021-06-17 13:49:13 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * *main - Start here
  *  *Return: Success prints 1
  */
 int main(void)
 {
-print("and that piece of art is useful,\" - Dora Korpar, 2015-10-19\n");
+write("and that piece of art is useful,\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
