35d86d63a249e238df457d09286daaf848f4562c
wuhibe <wuhibetamire710@gmail.com>
2021-06-18 12:21:45 -0700

    fixed multiple declarations

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 * *main - Starts here
 *  *Return: Always 0
@@ -9,32 +8,18 @@
 
 int main(void)
 {
-int n;
+		int n;
+		int x;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
 
-int y;
-y = n % 10;
-int x;
-x = abs(y);
-if (x > 5)
-{
-printf("Last digit of %d is ", n);
-printf("%d and is greater than 5\n", x);
-}
-else
-{
-if (x == 0)
-{
-printf("Last digit of %d is ", n);
-printf("%d and is 0\n", x);
-}
-else
-{
-printf("Last digit of %d is ", n);
-printf("%d and is less than 6 and not 0\n", x);
-}
-}
-return (0);
+		x = n % 10;
+		if (x > 5)
+			printf("Last digit of %d is %d and is greater than 5\n", n, x);
+		else if (ln == 0)
+			printf("Last digit of %d is %d and is 0\n", n, x);
+		else
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+		return (0);
 }
