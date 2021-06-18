3cee1a84878e2ccdf40d580769bb3d6fa4e223f0
wuhibe <wuhibetamire710@gmail.com>
2021-06-18 11:10:38 -0700

    fixed spacing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if(n<0)
-	{printf("$d is negative",n);}
-	
+	if (n < 0)
+	{
+	  printf ("%d is negative", n);
+	}
 	else
 	  {
-	    if(n=0)
-	      {printf("$d is zero",n);}
+	    if (n == 0)
+	      {
+		printf ("%d is zero", n);
+	      }
 	    else
-	      {printf("%d is positive",n);}
+	      {
+		printf ("%d is positive", n);
+	      }
 	  }
 
 	return (0);
