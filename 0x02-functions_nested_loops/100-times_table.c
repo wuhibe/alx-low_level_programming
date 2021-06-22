c26f7fe952072b28c25cd4744162bdf73af0a491
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 12:53:14 -0700

    Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
 *print_times_table - that prints the 9 times table
 *@n: variable taken
@@ -17,13 +18,11 @@ _putchar(44);
 _putchar(32);
 if (c <= n)
 {
-_putchar(32);
-_putchar(c + 48);
+printf("%i ", c);
 }
 else
 {
-_putchar((c / 10) + 48);
-_putchar((c % 10) + 48);
+printf("%i ", c)
 }
 }
 _putchar('\n');
