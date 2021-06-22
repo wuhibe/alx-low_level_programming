28c2b6db8ae5f3d4d714e359097c9f247f1a1ce7
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 10:24:49 -0700

    Print Last Digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_last_digit - Code to print last digit of number
+ *
+ *@r: variable to check
+ *
+ *Return: Always 0.
+ */
+int print_last_digit(int r)
+{
+int x;
+x = r % 10;
+return (x);
+}
