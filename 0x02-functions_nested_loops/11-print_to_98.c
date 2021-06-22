c8b74d063f6bbfb610602627694fbefceba7fe03
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 12:21:22 -0700

    Until 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,20 @@ void print_to_98(int n)
 {
 if (n > 98)
 {
-for (; n >= 98; n--)
+for (; n > 98; n--)
 {
 printf("%i, ", n);
+printf("98\n");
 }
+}
+else if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf("%i, ", n);
+printf("98\n");
 }
 else
-{
-for (; n <= 98; n++)
-{
-printf("%i, ", n);
-}
+printf("98\n");
 }
 }
