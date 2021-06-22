f2a70a3ce26c5fdb4c50f68262f175250c617069
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 10:01:18 -0700

    Check Positivity of int

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *print_sign - Code to check sign of int
+ *
+ *@n: variable to check
+ *
+ *Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+_putchar("+");
+return (1);
+}
+
+else if (n == 0)
+{
+_putchar("0");
+return (0);
+}
+
+else
+{
+_putchar("-");
+return (-1);
+}
+}
