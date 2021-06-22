5a105d0a98dc67cf04dc5493949b13d1617ef3a3
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 10:14:11 -0700

    Check absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.c"
+/**
+ *_abs - Calculate absolute value of int
+ *
+ *@r: variable to check absolute value
+ *
+ *Return: Always 0.
+ */
+int _abs(int r)
+{
+if (r < 0)
+return (-r);
+else
+return (r);
+}
