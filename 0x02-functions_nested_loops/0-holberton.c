f67763cc901e6cbe1910857a2479cdb2e83c7245
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 07:54:07 -0700

    Header file and First task

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - program starts here
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int x = 0;
+char a;
+char b[] = "Holberton\n";
+
+while (x < 10)
+{
+a = b[x];
+_putchar(a);
+i++;
+}
+return (0);
+}
