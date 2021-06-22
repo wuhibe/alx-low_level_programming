aecb7dade6e53d9c443371d38d46337d40c7fae4
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 09:04:31 -0700

    Print 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - program starts here
+ *
+ * Return: always 0
+ */
+int print_alphabet_x10(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar("\n");
+}
+return (0);
+}
