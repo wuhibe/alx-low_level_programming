3e56f154e24980b62c6dfd50e8a1fee3a3cfec42
wuhibe <wuhibetamire710@gmail.com>
2021-06-22 11:25:23 -0700

    Jack, Mult9, ADD

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* add - adds two integers and returns the result.
+*
+* @a: var one.
+* @b: var two.
+*
+* Return: Sum of two vars.
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,15 @@ for (h = 0; h < 24; h++)
 x = h / 10;
 y = h % 10;
 for (m = 0; m < 60; m++)
-	{
-	a = m / 10;
-	b = m % 10;
-	_putchar('0' + x);
-	_putchar('0' + y);
-	_putchar(':');
-	_putchar('0' + a);
-	_putchar('0' + b);
-	_putchar('\n');
-	}
+{
+a = m / 10;
+b = m % 10;
+_putchar('0' + x);
+_putchar('0' + y);
+_putchar(':');
+_putchar('0' + a);
+_putchar('0' + b);
+_putchar('\n');
+}
 }
 }
