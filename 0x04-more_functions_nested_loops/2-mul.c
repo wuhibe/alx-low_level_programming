35484ff032d8eae99bcdd5dceee7189a5bfa74d1
wuhibe <wuhibetamire710@gmail.com>
2021-06-24 08:30:48 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - Function to multiply two variables
+ *
+ * @a,@b: Variables to test
+ *
+ * Return: Product of the two variables.
+ */
+int mul(int a, int b)
+{
+_putchar('%d', (a * b));
+return (c);
+}
