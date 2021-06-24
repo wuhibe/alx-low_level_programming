e7ddaa415704b8fb0f73b4d46908978b257a3781
wuhibe <wuhibetamire710@gmail.com>
2021-06-24 08:38:07 -0700

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,16 @@
 /**
  * mul - Function to multiply two variables
  *
- * @a,@b: Variables to test
+ * @a: Variable 1
+ * @b: Variable 2
  *
  * Return: Product of the two variables.
  */
 int mul(int a, int b)
 {
 int c;
+
 c = a * b
+
 return (c);
 }
