18c1bd2f81e944cab259d7515cacde17301b7c24
wuhibe <wuhibetamire710@gmail.com>
2021-06-24 10:33:56 -0700

    Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,12 +9,22 @@
 void print_diagonal(int n)
 {
 char i;
+char x;
 if (n > 0)
 {
 for (i = 0; i < n; i++)
+{
+for (x = 0; x <= i; x++)
+{
+if (i != x)
 _putchar(' ');
+else
+{
 _putchar('\\');
 _putchar('\n');
+}
+}
+}
 }
 else
 _putchar('\n');
