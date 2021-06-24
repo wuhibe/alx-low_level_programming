7ef82ecc29e4d30679169dc6da56140c6f2f28a7
wuhibe <wuhibetamire710@gmail.com>
2021-06-24 09:58:11 -0700

    Print Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_line - Function to print a line of n length
+ *
+ * @n: Variable for length of line
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int i;
+if (i > 0)
+{
+for (i = 0; i < n; i++)
+_putchar('_');
+
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
