8b37d3b3357e1efeb40fb649cda065918788c7f8
wuhibe <wuhibetamire710@gmail.com>
2021-06-24 12:01:09 -0700

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,12 @@ void print_triangle(int size)
 int a, b, c;
 if (size <= 0)
 _putchar('\n');
+
 for (a = 0; a < size; a++)
 {
 for (b = size - a; b > 1; b--)
 _putchar(' ');
-for (c = row + column; c >= 1; c--)
+for (c = a + b; c >= 1; c--)
 _putchar('#');
 _putchar('\n');
 }
