55c17ff1aa8b58f9887d6070301a198d11c2ca90
wuhibe <wuhibetamire710@gmail.com>
2021-06-28 11:26:51 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_rev - Function to print strings in reverse.
+ *
+ * @s: Input string
+ */
+void print_rev(char *s)
+{
+int str;
+for (str = str - 1; str >= 0; str--)
+{
+_putchar(s[str]);
+}
+_putchar('\n');
+
+}
