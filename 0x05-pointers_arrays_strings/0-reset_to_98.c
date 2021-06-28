147fb454138cee4b2773404c396d92da8c426dd4
wuhibe <wuhibetamire710@gmail.com>
2021-06-28 09:58:00 -0700

    First Task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Function to reset value of n to 98.
+ *
+ * @n: variable received from main
+ *
+ * Return: None.
+ */
+void reset_to_98(int *n)
+{
+int *n = 98;
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
