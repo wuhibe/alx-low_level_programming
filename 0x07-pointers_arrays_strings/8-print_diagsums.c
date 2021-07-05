46a69be2bcb8bdd67d53da4569ffff4d7bb3bace
wuhibe <wuhibetamire710@gmail.com>
2021-07-05 09:56:32 -0700

    Add diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
  * print_diagsums - Function to add diagonal variables
  * @a: list of elements of matrix
@@ -7,13 +8,13 @@
  */
 void print_diagsums(int (*a), int size)
 {
-  int q, i, j;
-  int k = 0;
-  int new[size][size];
-  int sum1 = 0;
-  int sum2 = 0;
-for(i = 0; i < size; i++)
-for(j = 0; j < size; j++)
+int q, i, j;
+int k = 0;
+int new[size][size];
+int sum1 = 0;
+int sum2 = 0;
+for (i = 0; i < size; i++)
+for (j = 0; j < size; j++)
 {
 new[i][j] = a[k];
 k = k + 1;
