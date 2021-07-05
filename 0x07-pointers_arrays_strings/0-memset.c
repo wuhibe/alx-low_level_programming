1d32b60d24df6ba3631439bd7ac7b02a92c9a242
wuhibe <wuhibetamire710@gmail.com>
2021-07-05 19:35:42 +0300

    Strings and memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memset - Function to fill mem with constant
+ * @s: memory location
+ * @b: char to fill
+ * @n: number of elements
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int x;
+for(x = 0; x < n; x++)
+{
+*s = b;
+s++;
+}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - locate char function
+ *
+ * @s: starting pointer.
+ * @c: char to search.
+ * Return: final location of pointer.
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for(i = 0; i < strlen(s); i++)
+{
+if (c == s[i])
+return (&s[i]);
+/}
+
+return (NULL);
+}
