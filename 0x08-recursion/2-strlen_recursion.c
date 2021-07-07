192febe506f3ddd2a12c50a462f183ec1317c21f
wuhibe <wuhibetamire710@gmail.com>
2021-07-07 09:06:12 -0700

    Fixed data type

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
- * _strlen_recursion - Function to return length of string
+0;276;0c * _strlen_recursion - Function to return length of string
  *
  * @s: String pointer
  *
  * Return: Length.
  */
-void _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
+else
 return (_strlen_recursion(s + 1) + 1);
 }
