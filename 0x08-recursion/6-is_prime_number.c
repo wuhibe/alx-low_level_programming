a95c1441bc118dc80932dd0abc02ee3ae7c6f0d4
wuhibe <wuhibetamire710@gmail.com>
2021-07-07 10:55:06 -0700

    Optimus Prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,10 @@
-  
 #include "holberton.h"
 /**
  * sqtRecursive - computes square root recursively
  * @n: given number
  * @m: comparison number
  * Return: 1 if not found sqrroot, else sqrroot
- **/
+ */
 int sqtRecursive(int n, int m)
 {
 	if (n <= 0)
@@ -18,7 +17,7 @@ int sqtRecursive(int n, int m)
  * _sqrt_recursion - finds the natural square root of a number
  * @n: given number
  * Return: square root of n or -1
- **/
+ */
 int _sqrt_recursion(int n)
 {
 	if (n == 1)
@@ -29,7 +28,7 @@ int _sqrt_recursion(int n)
  * is_prime_number - checks if a given number is prime
  * @n: given number
  * Return: 1 if number is prime else 0
- **/
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1 || _sqrt_recursion(n) >= 1)
