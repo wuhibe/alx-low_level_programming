95deb6e6d6b9251571517472180533e57c63c945
wuhibe <wuhibetamire710@gmail.com>
2021-07-07 09:08:49 -0700

    Fixed betty issues

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-0;276;0c * _strlen_recursion - Function to return length of string
+ * _strlen_recursion - Function to return length of string
  *
  * @s: String pointer
  *
