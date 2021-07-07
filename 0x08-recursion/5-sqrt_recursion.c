69015c847f97a5b65c247cfa7032672375f28141
wuhibe <wuhibetamire710@gmail.com>
2021-07-07 10:18:02 -0700

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.c"
+#include "holberton.h"
 /**
  * _sqrt_recursion - function to return sqrt of a num
  *
@@ -9,21 +9,35 @@
 int _sqrt_recursion(int n)
 {
 	int x = 2;
+
 	if (n < 0)
 		return (-1);
+
 	else if (n == 0 || n == 1)
 		return (n);
+
 	else
+	{
 		chksrqrt(n, x);
-	return (x);
+		return (x);
+	}
 }
 
+/**
+ * chksqrt - find square root.
+ * @n: main var.
+ * @x: square root var.
+ * Return: Variable x.
+ */
 int chksqrt(int n, int x)
 {
 	if (n == (x * x))
 		return (x);
 	else if (n > (x * x))
+	{
 		return (-1);
+	}
+
 	else
 		chksqrt(n, (x + 1))
 }
