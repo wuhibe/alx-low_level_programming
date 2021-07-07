8130f942d31755bdf89044748a4a1e7e033bec06
wuhibe <wuhibetamire710@gmail.com>
2021-07-07 10:09:37 -0700

    Sruare root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.c"
+/**
+ * _sqrt_recursion - function to return sqrt of a num
+ *
+ * @n: num to check.
+ *
+ * Return: the sqrt.
+ */
+int _sqrt_recursion(int n)
+{
+	int x = 2;
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		chksrqrt(n, x);
+	return (x);
+}
+
+int chksqrt(int n, int x)
+{
+	if (n == (x * x))
+		return (x);
+	else if (n > (x * x))
+		return (-1);
+	else
+		chksqrt(n, (x + 1))
+}
