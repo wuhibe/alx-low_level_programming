5c2812eb2599bedceacfe7efc3f76897b2318f87
wuhibe <wuhibetamire710@gmail.com>
2021-07-12 22:12:11 +0300

    Final Push

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+/**
+ * check - function to check number.
+ * @s: string recieved from main
+ * Return: 0 or 1.
+ */
+int check(char *s)
+{
+	int i = 0, j = strlen(s);
+
+	while (i < j)
+	{
+		{
+			if (s[i] < '0' || s[i] > '9')
+				return (1);
+
+			else
+				i++;
+		}
+
+	}
+	return (0);
+}
+
 /**
  * main - starting func to calculate change
  *
@@ -17,7 +41,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
+	if (check(argv[1]))
+	{
+		printf("Error\n");
+		return (1);
+	}
 	x = x + atoi(argv[1]);
 
 	if (x >= 25)
