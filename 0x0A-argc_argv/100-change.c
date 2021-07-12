26ce193fdc7b953a0c58b50964561daa4357515b
wuhibe <wuhibetamire710@gmail.com>
2021-07-12 22:32:45 +0300

    Fixed lines issue wth betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,8 +3,8 @@
 /**
  * main - starting func to calculate change
  *
- * @argc - count of args
- * @argv - value of args
+ * @argc: count of args
+ * @argv: value of args
  *
  * Return: Always 0.
  */
@@ -18,58 +18,21 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	x = x + atoi(argv[1]);
-	if (x < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
 	if (x >= 25)
-	{
 		while (x >= 25)
-		{
-			change += 1;
-			x = x - 25;
-		}
-	}
-
+			change ++, x -= 25;
 	if (x >= 10)
-	{
 		while (x >= 10)
-		{
-			change += 1;
-			x = x - 10;
-		}
-	}
+			change ++, x -= 10;
 	if (x >= 5)
-	{
 		while (x >= 5)
-		{
-			change += 1;
-			x = x - 5;
-		}
-	}
-
+			change ++, x -= 5;
 	if (x >= 2)
-	{
 		while (x >= 2)
-		{
-			change += 1;
-			x = x - 2;
-		}
-	}
-
+			change ++, x -= 2;
 	if (x >= 1)
-	{
 		while (x >= 1)
-		{
-			change += 1;
-			x--;
-		}
-	}
-
-
+			change ++, x--;
 	printf("%d\n", change);
-
 	return (0);
 }
