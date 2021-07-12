c3bf490c92f171d395092eadbafbe8502f01b86a
wuhibe <wuhibetamire710@gmail.com>
2021-07-12 14:18:50 +0300

    Fixed issue of unused vars

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,13 @@
  * main - Starting function to print name of program.
  * @argc: count of args.
  * @argv: vector value taken from command line.
- * Return: Always 0.
+ * Return: argc.
  */
 int main(int argc, char *argv[])
 {
+	int x;
+
+	x = argc;
 	printf("%s\n", argv[0]);
-	return (0);
+	return (x);
 }
