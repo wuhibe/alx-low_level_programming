06763a6915ad6c41e52992fe1ea812a6c42d236c
wuhibe <wuhibetamire710@gmail.com>
2021-07-12 20:31:34 +0300

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+/**
+ * check - function to check number.
+ * @s: string recieved from main
+ * Return: 0 or 1.
+ */
+int check(char *s)
+{
+	int i = 0, j = strlen(s);
+
+	while (i < j)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		else
+			return (1);
+	}
+}
+
 /**
  * main - Starting function to multiply two numbers.
  * @argc: count of args.
@@ -12,12 +31,11 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if ((atoi(argv[i])))
+		if (check(argv[i]))
 		{
 			sum += atoi(argv[i]);
 		}
-
-	       else
+		else
 		{
 			printf("Error\n");
 			return (1);
