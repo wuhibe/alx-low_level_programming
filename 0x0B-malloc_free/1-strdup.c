cbc69002448ac99a1871aa6117c250d348705369
wuhibe <wuhibetamire710@gmail.com>
2021-07-13 20:14:07 +0300

    Duplicate + Concatenate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strdup - returns a pointer to a newly allocated space with copy of str
+ * @str: string to copy
+ * Return: pointer to array or null
+ **/
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	i = i + 1;
+	s = malloc(sizeof(*str) * i);
+	if (s == NULL)
+		return (NULL);
+	while (str[j] != '\0')
+		s[j] = str[j], j++;
+	return (s);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * str_concat - function to concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new space in memory or null
+ **/
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+		s[i] = s1[i], i++;
+	while (s2[j] != '\0')
+		s[i] = s2[j], i++, j++;
+	s[i] = '\0';
+	return (s);
+}
