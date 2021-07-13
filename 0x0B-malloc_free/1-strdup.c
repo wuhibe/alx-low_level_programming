bc3dc749de4334cacf8fa97186a7e0e74e30a072
wuhibe <wuhibetamire710@gmail.com>
2021-07-13 20:18:36 +0300

    Fixed issue of libraries

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
  * _strdup - returns a pointer to a newly allocated space with copy of str
  * @str: string to copy
@@ -9,14 +10,14 @@ char *_strdup(char *str)
 	char *s;
 	int i = 0, j = 0;
 
-	if (str == NULL)
-		return (NULL);
+	if (str == '\0')
+		return ('\0');
 	while (str[i] != '\0')
 		i++;
 	i = i + 1;
 	s = malloc(sizeof(*str) * i);
-	if (s == NULL)
-		return (NULL);
+	if (s == '\0')
+		return ('\0');
 	while (str[j] != '\0')
 		s[j] = str[j], j++;
 	return (s);
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
  * str_concat - function to concatenate two strings
  * @s1: first string
@@ -10,17 +11,17 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i = 0, j = 0;
 
-	if (s1 == NULL)
+	if (s1 == '\0')
 		s1 = "";
-	if (s2 == NULL)
+	if (s2 == '\0')
 		s2 = "";
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
 		j++;
 	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
-		return (NULL);
+	if (s == '\0')
+		return ('\0');
 	while (s1[i] != '\0')
 		s[i] = s1[i], i++;
 	while (s2[j] != '\0')
