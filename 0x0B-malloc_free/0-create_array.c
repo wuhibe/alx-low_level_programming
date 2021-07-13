0dccf07102478fab355283894f65d0c0f9433c38
wuhibe <wuhibetamire710@gmail.com>
2021-07-13 19:56:15 +0300

    First push

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *create_array - function that creates an array of chars, and initializes it with a specific char.
+ * @size: size of the array
+ * @c: char to be initialized with
+ * Return: pointer to array or null
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+}
