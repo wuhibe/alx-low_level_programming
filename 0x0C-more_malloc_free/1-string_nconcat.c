f37cc94035bc951dfbd888b93e8628b1d1b6b096
wuhibe <wuhibetamire710@gmail.com>
2021-07-16 00:42:52 +0300

    Final try before bed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,6 +21,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1[i] != '\0')
 		i++;
 	ptr = (char *)malloc(sizeof(char) * (i + n + 1));
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 	while (s2[j] != '\0')
