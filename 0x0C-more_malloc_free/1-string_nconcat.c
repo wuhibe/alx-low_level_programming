7aa3661a23ea4c8879bbe690b55d919672d7ed7e
wuhibe <wuhibetamire710@gmail.com>
2021-07-15 21:40:46 +0300

    HELP

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,31 +12,27 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k;
-	char *strDup;
+	char *concat;
 
 	if (s1 == NULL)
 		i = 0;
 	else
-	{
 		for (i = 0; s1[i]; i++)
 			;
-	}
 	if (s2 == NULL)
 		j = 0;
 	else
-	{
 		for (j = 0; s2[j]; j++)
 			;
-	}
 	if (j > n)
 		j = n;
-	strDup = malloc(sizeof(char) * (i + j + 1));
-	if (strDup == NULL)
+	concat = malloc(sizeof(char) * (i + j + 1));
+	if (concat == NULL)
 		return (NULL);
 	for (k = 0; k < i; k++)
-		strDup[k] = s1[k];
+		concat[k] = s1[k];
 	for (k = 0; k < j; k++)
-		strDup[k + i] = s2[k];
-	strDup[i + j] = '\0';
-	return (strDup);
+		concat[k + i] = s2[k];
+	concat[i + j] = '\0';
+	return (concat);
 }
