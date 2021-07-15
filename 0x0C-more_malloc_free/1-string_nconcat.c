dce35973d6c1cd8b200b562ed7baa73230463f71
wuhibe <wuhibetamire710@gmail.com>
2021-07-16 00:01:47 +0300

    Added functionality for NULL string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,14 +15,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 
 	if (s1 == NULL)
-		i = 0;
+		s1 = "";
 	if (s2 == NULL)
-		j = 0;
+		s2 = "";
 	while (s1[i] != '\0')
 		i++;
 	ptr = (char *)malloc(sizeof(char) * (i + n + 1));
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
+	while(s2[j] != '\0')
+		j++;
+	if (j > 0)
+		j = n;
+	else
+		n = j;
 	for (j = 0; j < n; j++)
 	{
 		ptr[(i + j)] = s2[j];
