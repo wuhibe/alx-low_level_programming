b2392701256d9f82bb0efb694428fed9dc78948d
wuhibe <wuhibetamire710@gmail.com>
2021-07-16 01:04:48 +0300

    Last push

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	memory = malloc(nmemb * size);
 	if (memory == NULL)
 		return (NULL);
-	for (i = 0; i <= (nmemb * size); i++)
+	for (i = 0; i < (nmemb * size); i++)
 		memory[i] = 0;
 	return (memory);
 }
