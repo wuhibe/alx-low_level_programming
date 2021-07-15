aba11eb935d659d3aa7d2e036c9a39cb5360674a
wuhibe <wuhibetamire710@gmail.com>
2021-07-16 00:38:53 +0300

    This is embarassing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,14 +25,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 	while (s2[j] != '\0')
 		j++;
-	if (j > 0)
+	if (j >= n)
 		j = n;
-	else
-		n = j;
 	for (j = 0; j < n; j++)
-	{
 		ptr[(i + j)] = s2[j];
-	}
-	ptr[i + j] = '\0';
+	ptr[(i + j)] = '\0';
 	return (ptr);
 }
