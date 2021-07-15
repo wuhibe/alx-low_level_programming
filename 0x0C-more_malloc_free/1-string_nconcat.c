6919083b5be83ef8d76ce0ebc46f679bd343a448
wuhibe <wuhibetamire710@gmail.com>
2021-07-15 19:55:58 +0300

    Attempt 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
-	strDup = malloc(sizeof(char) * (i + n + 1));
+	strDup = malloc(sizeof(char) * (i + n));
 	if (strDup == NULL)
 		return (NULL);
 	i = j = 0;
