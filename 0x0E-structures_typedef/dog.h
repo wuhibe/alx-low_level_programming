b6d9e6fb8851a85c243c564b83bea660c90e9957
wuhibe <wuhibetamire710@gmail.com>
2021-07-19 21:15:43 +0300

    DOG

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a new type struct to define dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
-	char *(name);
+	char *name;
 	float age;
 	char *owner;
 };
