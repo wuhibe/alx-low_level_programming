4634319b4694a7bf5bfca2909c79639fd1830f7f
wuhibe <wuhibetamire710@gmail.com>
2021-07-22 20:32:27 +0300

    Check 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,6 +14,7 @@ typedef struct id_func
 	void (*f)(va_list);
 } print_id;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
