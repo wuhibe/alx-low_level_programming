a7c5a2104c7a54612cc491b33e7cb0d20f8153b2
wuhibe <wuhibetamire710@gmail.com>
2021-08-03 23:59:10 +0300

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,10 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr;
-
-	if (head == NULL || *head == NULL)
-		return;
-	while (*head != NULL)
+       	if (*head != NULL)
 	{
-		ptr = *head;
+		free_listint2(&(*head)->next);
 		*head = (*head)->next;
-		free(ptr);
 	}
 	head = NULL;
 }
