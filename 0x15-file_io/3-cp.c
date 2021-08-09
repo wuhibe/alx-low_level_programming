81b674ba42741db095031e44261b33fb62bca057
wuhibe <wuhibetamire710@gmail.com>
2021-08-09 22:03:32 +0300

    Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,74 +1,85 @@
+#include <stdio.h>
 #include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
- * main - the entry point for the program
- * @argc: the number of argument
- * @argv: the array of an argument
- *
- * Return: sucess always
- **/
-int main(int argc, char *argv[])
-{
-char *buff;
-int fdfrom, fdto, fdw, fdr;
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-buff = create_buffer(argv[2]);
-fdfrom = open(argv[1], O_RDONLY);
-fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-fdr = read(fdfrom, buff, 1024);
-do {
-if (fdr == -1 || fdfrom == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buff);
-exit(98);
-}
-fdw = write(fdto, buff, fdr);
-if (fdw == -1 || fdw != fdr || fdto == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buff);
-exit(99);
-}
-fdr = read(fdfrom, buff, 1024);
-fdto = open(argv[2], O_WRONLY | O_APPEND);
-} while (fdr > 0);
-free(buff);
-close_file(fdfrom);
-close_file(fdto);
-return (0);
-}
-/**
- * close_file - close a file
- * @fd: file descriptor
- **/
-void close_file(int fd)
-{
-int fdc;
-fdc = close(fd);
-if (fdc == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-}
-/**
- * create_buffer - create buffer
- * @f: the file name
- *
- * Return: the buffer
+ * _error - Read file.
+ * @e: Error number
+ * @filename: File name
  */
-char *create_buffer(char *f)
+void _error(int e, char *filename)
 {
-char *buff;
-buff = malloc(sizeof(char) * 1024);
-if (buff == NULL)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
-exit(99);
+	if (e == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	if (e == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
-return (buff);
+/**
+ * cp - Copies the content of a file to another file.
+ * @file_from: Name of the source file.
+ * @file_to: Name of the destination file.
+ * Return: 1 on success, -1 on failure.
+ */
+void cp(char *file_from, char *file_to)
+{
+	int fd_read, res_read, fd_write, res_write;
+	char *buf[1024];
+
+	/* READ */
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read < 0)
+		_error(98, file_from);
+	/* WRITE */
+	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_write < 0)
+	{
+		close(fd_read);
+		_error(99, file_to);
+	}
+	do {
+		/* READ */
+		res_read = read(fd_read, buf, 1024);
+		if (res_read < 0)
+			_error(98, file_from);
+		/* WRITE */
+		res_write = write(fd_write, buf, res_read);
+		if (res_write < res_read)
+			_error(99, file_to);
+	}	while (res_write == 1024);
+	if (close(fd_read) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+		close(fd_write);
+		exit(100);
+	}
+	if (close(fd_write) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+		exit(100);
+	}
+}
+/**
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count
+ * @av: argument values
+ * Return: 0 on succes, -1 on error.
+ */
+int main(int ac, char *av[])
+{
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	cp(av[1], av[2]);
+	return (0);
 }
