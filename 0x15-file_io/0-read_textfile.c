fbba712e29e2a6e7a416b649b4c1b7ab6ead21bc
wuhibe <wuhibetamire710@gmail.com>
2021-08-09 22:05:41 +0300

    Read/Write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,43 @@
+#include <stdio.h>
 #include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
- * read_textfile - function to read a text and write to POSIX stdout
- * @filename: name of file
- * @letters: count of characters to read/write
- * Return: count of written characters
+ * read_textfile - Reads a text file and prints it to the POSIX
+ * standard output.
+ * @filename: file.
+ * @letters: Number of letters it should read and print.
+ * Return: Actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, readText, writeText;
-	char *buffer;
+	int fd, res_read, res_write;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-
-	readText = read(fd, buffer, letters);
-	if (readText == -1)
+	res_read = read(fd, buf, letters);
+	if (res_read == -1)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-
-	writeText = write(STDOUT_FILENO, buffer, readText);
-	if (writeText == -1 || readText != writeText)
+	res_write = write(STDOUT_FILENO, buf, res_read);
+	if (res_write == -1 || res_read != res_write)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-
-	free(buffer);
+	free(buf);
 	close(fd);
-	return (writeText);
+	return (res_write);
 }
