f1f170eb3c26f4d61b2c38afd9bac42947a27af0
wuhibe <wuhibetamire710@gmail.com>
2021-08-10 09:46:56 +0300

    Headers

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -10,8 +10,12 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);
-int append_text_to_file(const char *filename, char *text_content);
-int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t l\
+etters);
+int append_text_to_file(const char *filename, char *\
+text_content);
+int create_file(const char *filename, char *text_con\
+tent);
 
+#endif
 #endif
