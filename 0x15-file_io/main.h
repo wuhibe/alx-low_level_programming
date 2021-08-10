66cbc5b913723b97120eb0c5c18e6479f4474776
wuhibe <wuhibetamire710@gmail.com>
2021-08-10 20:28:08 +0300

    Again

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,22 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
 
-typedef unsigned int ssize_t;
 int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t l\
-etters);
-int append_text_to_file(const char *filename, char *\
-text_content);
-int create_file(const char *filename, char *text_con\
-tent);
 
-#endif
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void close_file(int fd);
+char *create_buffer(char *f);
+
 #endif
